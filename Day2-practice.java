812a7e0f977e29abdd06730b374619e6f8deb019
Mohan Kumar M <mohankumar04theni@gmail.com>
2025-06-11 21:24:20 +0530

    Add files via upload

---
+++ Day2-practice.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int age = 21;
+	    float height = 5.6f;
+	    double pi = 3.1415926535;
+	    char grade = 'A';
+	    Boolean isPassed = true;
+	    String name = "Mohan Kumar";
+	
+    System.out.println("My Age is "+age);
+	System.out.println("My Height is "+height);
+	System.out.println("P : "+pi);
+	System.out.println("My Grade is "+grade);
+	System.out.println("Is student passed ? "+isPassed);
+	}
+}
---
+++ StudentInfo.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+public class StudentInfo {
+    public static void main (String[] args) {
+        String studentName = ("Mohan Kumar");
+        int mark = 90;
+        float attendence = 90f;
+        boolean isEligibleFor= true;
+        System.out.println( "StudentName : "+ studentName);
+        System.out.println( "Mark : "+ mark);
+        System.out.println( "Attendence : "+ attendence + "%");
+        System.out.println( "Is Student Eligible  : "+ isEligibleFor);
+        
+    }
+}
